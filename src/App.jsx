0d56bfaee0d6725c4df273408539ceae7a75769d
Fortune Ekeruo <legalmody@gmail.com>
2020-12-02 01:40:46 +0100

    fix lint and add some styles

--- src/App.jsx
+++ src/App.jsx
@@ -13,53 +13,61 @@ const App = () => {
   const loadFfmpeg = async () => {
     await mpeg.load();
     setIsLoaded(true);
-  }
+  };
 
   useEffect(() => {
     loadFfmpeg();
-  }, [])
+  }, []);
 
   const convertToGif = async () => {
     mpeg.FS('writeFile', 'sample.mp4', await fetchFile(video));
-    await mpeg.run('-i', 'sample.mp4', '-t', '2.5', '-ss', '2.0', '-f', 'gif', 'result.gif');
+    await mpeg.run(
+      '-i',
+      'sample.mp4',
+      '-t',
+      '2.5',
+      '-ss',
+      '2.0',
+      '-f',
+      'gif',
+      'result.gif',
+    );
 
     const data = mpeg.FS('readFile', 'result.gif');
-    const gifUrl = URL.createObjectURL(new Blob([data.buffer], { type: 'image/gif'}));
-  
+    const gifUrl = URL.createObjectURL(
+      new Blob([data.buffer], { type: 'image/gif' }),
+    );
+
     setGif(gifUrl);
-  }
+  };
 
   return isLoaded ? (
-      <div className="App">
-        <h1>Please choose a video to convert</h1>
-        { video ? 
-          <div>
-            <video
-              controls
-              width="320"
-              src={URL.createObjectURL(video)}
-            />
-            <br />
-            <button class="btn" onClick={convertToGif}>Convert</button>
-          </div> :
-          <input
-            type="file"
-            onChange={e => setVideo(e.target.files?.item(0))}
-          />
-        }
-
-        <div style={{ marginTop: '20px' }}>
-          <h1>Result</h1>
-
-          <img src={gif} />
+    <div className="App">
+      <h1>Please choose a video to convert</h1>
+      {video ? (
+        <div>
+          <video controls width="320" src={URL.createObjectURL(video)} />
+          <br />
+          <button class="btn" onClick={convertToGif}>
+            Convert
+          </button>
         </div>
+      ) : (
+        <input
+          type="file"
+          onChange={(e) => setVideo(e.target.files?.item(0))}
+        />
+      )}
 
-        
+      <div style={{ marginTop: '20px' }}>
+        <h1>Result</h1>
+
+        <img src={gif} />
       </div>
-    ) :
-    (
-      <p style={{ textAlign: 'center' }}>Loading...</p>
-    );
-}
+    </div>
+  ) : (
+    <p>Loading...</p>
+  );
+};
 
 export default App;
